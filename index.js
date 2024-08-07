f11dff1b500539284edd3a3ac38a22c268e59c60
Saksham Singh Bisht <118253416+saksha914@users.noreply.github.com>
2024-08-08 01:29:31 +0530

    add mongoDb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const routes = require("./routes");
+const connectDB = require("./Db/db");
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+connectDB();
 
 // Use the routes handling function
 routes(app);
