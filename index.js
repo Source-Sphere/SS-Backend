0efe5b681e189c5db2652a593c51ddcde077e280
Saksham Singh Bisht <118253416+saksha914@users.noreply.github.com>
2024-08-09 15:12:17 +0530

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const routes = require("./routes");
 const connectDB = require("./Db/db");
-const router = express.router;
 
 dotenv.config();
 
@@ -10,7 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/.netlify/funtion/routes", router);
 connectDB();
 
 routes(app);
@@ -18,5 +16,3 @@ routes(app);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-module.exports.handler = serverless(app);
