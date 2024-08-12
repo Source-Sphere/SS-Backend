f5b80ffc66234af57fdbfa9ea1612b0cfa8be198
Saksham Singh Bisht <118253416+saksha914@users.noreply.github.com>
2024-08-12 18:57:28 +0530

    implemented jwt

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,13 +1,20 @@
+// routes/userRoutes.js
+
 const express = require("express");
 const router = express.Router();
 const userService = require("../services/usersService");
 
-router.get("/get_user", async (req, res) => {
+router.post("/create_user", async (req, res) => {
   try {
-    const users = await userService.getUsers();
-    res.status(200).json(users);
+    const response = await userService.createUser(req);
+
+    if (response.status === 400) {
+      return res.status(400).json({ message: response.message });
+    }
+
+    res.status(201).json(response.data);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: "Server Error" });
   }
 });
 
