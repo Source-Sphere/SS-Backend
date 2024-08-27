2ac86adbf3ee7757886fd3fc9d1fe4e73014dfc9
Saksham Singh Bisht <118253416+saksha914@users.noreply.github.com>
2024-08-27 19:39:03 +0530

    added org routes and service

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const testRoutes = require("./routes/testRoutes");
 const userRoutes = require("./routes/usersRoute");
-const projectRoutes = require("./routes/projectRoutes")
+const projectRoutes = require("./routes/projectRoutes");
+const orgRoutes = require("./routes/orgRoutes");
 
 module.exports = (app) => {
   app.use("/test", testRoutes);
   app.use("/user", userRoutes);
-  app.use("/project",projectRoutes);
+  app.use("/project", projectRoutes);
+  app.use("/org", orgRoutes);
 };
